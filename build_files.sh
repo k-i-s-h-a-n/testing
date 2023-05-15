6b92683f732ff86a120304ad355db675e3f7dcf5
k-i-s-h-a-n <kishfan86@gmail.com>
2023-05-15 18:07:16 +0530

    qswd

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
+# Install project dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --noinput
 
+# Additional deployment steps or commands can be added here
 
+# Run the project
+python manage.py runserver
