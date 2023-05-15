e68ba2f8ae75603fb031b2ab47795e2a2079d598
k-i-s-h-a-n <kishfan86@gmail.com>
2023-05-15 18:14:47 +0530

    finaltry3

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Create and activate a virtual environment
+python3 -m venv myenv
+source myenv/bin/activate
+
 # Install project dependencies
 pip install -r requirements.txt
 
@@ -7,6 +11,9 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 # Additional deployment steps or commands can be added here
+
+# Deactivate the virtual environment
+deactivate
 
 # Run the project
 python manage.py runserver
